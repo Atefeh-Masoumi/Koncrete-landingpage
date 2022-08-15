a22d84bda608858f9b7c21dc4015f2dfb986c3d6
Atefeh Masoumi <atefeh.masoomy@gmail.com>
2022-08-15 10:21:47 +0430

    build completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Body from './components/Body/Body';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 
-// https://danapeyvast.com/products/koncrete-wp-theme/landing/
+
 function App() {
   return (
     <>
